daa0324bdfb8379abb6e17593ea57403c8459f8b
Sullivan008 <kozakpeter0424@gmail.com>
2021-11-19 11:30:52 +0100

    * Modified every file to the the file scoped using in the Application.Client.Common project. * Removed unused usings.

--- Application.Client.Common/Commands/AsyncCommandBase.cs
+++ Application.Client.Common/Commands/AsyncCommandBase.cs
@@ -1,101 +1,98 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Application.Client.Common.ViewModels;
 using System.Windows.Input;
-using Application.Client.Common.ViewModels;
 
-namespace Application.Client.Common.Commands
+namespace Application.Client.Common.Commands;
+
+public abstract class AsyncCommandBase<TCallerViewModel> : ICommand where TCallerViewModel : ViewModelBase
 {
-    public abstract class AsyncCommandBase<TCallerViewModel> : ICommand where TCallerViewModel : ViewModelBase
+    public virtual Predicate<object?>? CanExecute { get; }
+
+    protected readonly TCallerViewModel CallerViewModel;
+
+    private bool _isExecuting;
+
+    protected AsyncCommandBase(TCallerViewModel callerViewModel)
     {
-        public virtual Predicate<object?>? CanExecute { get; }
-
-        protected readonly TCallerViewModel CallerViewModel;
-
-        private bool _isExecuting;
-
-        protected AsyncCommandBase(TCallerViewModel callerViewModel)
-        {
-            CanExecute = default;
-            CallerViewModel = callerViewModel ?? throw new ArgumentNullException(nameof(callerViewModel));
-        }
-
-        public event EventHandler? CanExecuteChanged
-        {
-            add => CommandManager.RequerySuggested += value;
-            remove => CommandManager.RequerySuggested -= value;
-        }
-
-        bool ICommand.CanExecute(object? parameter)
-        {
-            if (!_isExecuting && CanExecute == null)
-            {
-                return true;
-            }
-
-            return !_isExecuting && CanExecute!(parameter);
-        }
-
-        async void ICommand.Execute(object? parameter)
-        {
-            _isExecuting = true;
-
-            try
-            {
-                await ExecuteAsync();
-            }
-            finally
-            {
-                _isExecuting = false;
-            }
-        }
-
-        public abstract Task ExecuteAsync();
+        CanExecute = default;
+        CallerViewModel = callerViewModel ?? throw new ArgumentNullException(nameof(callerViewModel));
     }
 
-    public abstract class AsyncCommandBase<TCallerViewModel, TCommandParameter> : ICommand where TCallerViewModel : ViewModelBase
+    public event EventHandler? CanExecuteChanged
     {
-        public virtual Predicate<object?>? CanExecute { get; }
-
-        protected readonly TCallerViewModel CallerViewModel;
-
-        private bool _isExecuting;
-
-        protected AsyncCommandBase(TCallerViewModel callerViewModel)
-        {
-            CanExecute = default;
-            CallerViewModel = callerViewModel ?? throw new ArgumentNullException(nameof(callerViewModel));
-        }
-
-        public event EventHandler? CanExecuteChanged
-        {
-            add => CommandManager.RequerySuggested += value;
-            remove => CommandManager.RequerySuggested -= value;
-        }
-
-        bool ICommand.CanExecute(object? parameter)
-        {
-            if (!_isExecuting && CanExecute == null)
-            {
-                return true;
-            }
-
-            return !_isExecuting && CanExecute!(parameter);
-        }
-
-        async void ICommand.Execute(object? parameter)
-        {
-            _isExecuting = true;
-
-            try
-            {
-                await ExecuteAsync((TCommandParameter) parameter!);
-            }
-            finally
-            {
-                _isExecuting = false;
-            }
-        }
-
-        public abstract Task ExecuteAsync(TCommandParameter parameter);
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
     }
+
+    bool ICommand.CanExecute(object? parameter)
+    {
+        if (!_isExecuting && CanExecute == null)
+        {
+            return true;
+        }
+
+        return !_isExecuting && CanExecute!(parameter);
+    }
+
+    async void ICommand.Execute(object? parameter)
+    {
+        _isExecuting = true;
+
+        try
+        {
+            await ExecuteAsync();
+        }
+        finally
+        {
+            _isExecuting = false;
+        }
+    }
+
+    public abstract Task ExecuteAsync();
+}
+
+public abstract class AsyncCommandBase<TCallerViewModel, TCommandParameter> : ICommand where TCallerViewModel : ViewModelBase
+{
+    public virtual Predicate<object?>? CanExecute { get; }
+
+    protected readonly TCallerViewModel CallerViewModel;
+
+    private bool _isExecuting;
+
+    protected AsyncCommandBase(TCallerViewModel callerViewModel)
+    {
+        CanExecute = default;
+        CallerViewModel = callerViewModel ?? throw new ArgumentNullException(nameof(callerViewModel));
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    bool ICommand.CanExecute(object? parameter)
+    {
+        if (!_isExecuting && CanExecute == null)
+        {
+            return true;
+        }
+
+        return !_isExecuting && CanExecute!(parameter);
+    }
+
+    async void ICommand.Execute(object? parameter)
+    {
+        _isExecuting = true;
+
+        try
+        {
+            await ExecuteAsync((TCommandParameter) parameter!);
+        }
+        finally
+        {
+            _isExecuting = false;
+        }
+    }
+
+    public abstract Task ExecuteAsync(TCommandParameter parameter);
 }
--- Application.Client.Common/ViewModels/ViewModelBase.cs
+++ Application.Client.Common/ViewModels/ViewModelBase.cs
@@ -1,19 +1,17 @@
-﻿#nullable enable
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Application.Client.Common.ViewModels
-{
-    public class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler? PropertyChanged;
+namespace Application.Client.Common.ViewModels;
 
-        public void OnPropertyChanged([CallerMemberName] string? name = null)
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        if (name != null)
         {
-            if (name != null)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
