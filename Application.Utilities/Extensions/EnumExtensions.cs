2f64fbb19116fcb14ee130a4c6c71b54aa2ea1a5
Sullivan008 <kozakpeter0424@gmail.com>
2021-11-19 11:26:02 +0100

    * Modified every file to the the file scoped using in the Application.Utilities project. * Removed unused usings.

--- Application.Utilities/Extensions/EnumExtensions.cs
+++ Application.Utilities/Extensions/EnumExtensions.cs
@@ -1,32 +1,29 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.Serialization;
 
-namespace Application.Utilities.Extensions
+namespace Application.Utilities.Extensions;
+
+public static class EnumExtension
 {
-    public static class EnumExtension
+    public static string GetEnumMemberAttrValue<TEnum>(this TEnum @this) where TEnum : Enum
     {
-        public static string GetEnumMemberAttrValue<TEnum>(this TEnum @this) where TEnum : Enum
-        {
-            EnumMemberAttribute enumMemberAttr = @this.GetAttribute<EnumMemberAttribute>();
+        EnumMemberAttribute enumMemberAttr = @this.GetAttribute<EnumMemberAttribute>();
 
-            Guard.Guard.ThrowIfNullOrWhitespace(enumMemberAttr.Value, nameof(enumMemberAttr.Value));
+        Guard.Guard.ThrowIfNullOrWhitespace(enumMemberAttr.Value, nameof(enumMemberAttr.Value));
 
-            return enumMemberAttr.Value!;
-        }
+        return enumMemberAttr.Value!;
+    }
 
-        private static TAttributeType GetAttribute<TAttributeType>(this Enum @this)
-        {
-            Type type = @this.GetType();
-            MemberInfo[] memberInfo = type.GetMember(@this.ToString());
+    private static TAttributeType GetAttribute<TAttributeType>(this Enum @this)
+    {
+        Type type = @this.GetType();
+        MemberInfo[] memberInfo = type.GetMember(@this.ToString());
 
-            object[] attributes = memberInfo[0].GetCustomAttributes(typeof(TAttributeType), false);
-            object? attribute = attributes.SingleOrDefault();
+        object[] attributes = memberInfo[0].GetCustomAttributes(typeof(TAttributeType), false);
+        object? attribute = attributes.SingleOrDefault();
 
-            Guard.Guard.ThrowIfNull(attribute, nameof(attribute));
+        Guard.Guard.ThrowIfNull(attribute, nameof(attribute));
 
-            return (TAttributeType)attribute!;
-        }
+        return (TAttributeType)attribute!;
     }
 }
--- Application.Utilities/Guard/Guard.cs
+++ Application.Utilities/Guard/Guard.cs
@@ -1,36 +1,33 @@
-﻿using System;
+﻿namespace Application.Utilities.Guard;
 
-namespace Application.Utilities.Guard
+public static class Guard
 {
-    public static class Guard
+    public static void ThrowIfNull<T>(T input, string parameterName, string? message = null)
     {
-        public static void ThrowIfNull<T>(T input, string parameterName, string? message = null)
+        switch (Type.GetTypeCode(typeof(T)))
         {
-            switch (Type.GetTypeCode(typeof(T)))
+            case TypeCode.String:
             {
-                case TypeCode.String:
+                ThrowIfNullOrWhitespace(input as string, parameterName, message);
+                break;
+            }
+            default:
+            {
+                if (input is null)
                 {
-                    ThrowIfNullOrWhitespace(input as string, parameterName, message);
-                    break;
+                    throw new ArgumentNullException(parameterName, message ?? "The value cannot be null!");
                 }
-                default:
-                {
-                    if (input is null)
-                    {
-                        throw new ArgumentNullException(parameterName, message ?? "The value cannot be null!");
-                    }
 
-                    break;
-                }
+                break;
             }
         }
+    }
         
-        public static void ThrowIfNullOrWhitespace(string? input, string parameterName, string? message = null)
+    public static void ThrowIfNullOrWhitespace(string? input, string parameterName, string? message = null)
+    {
+        if (string.IsNullOrWhiteSpace(input))
         {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                throw new ArgumentNullException(parameterName, message ?? "The value cannot be null!");
-            }
+            throw new ArgumentNullException(parameterName, message ?? "The value cannot be null!");
         }
     }
 }
