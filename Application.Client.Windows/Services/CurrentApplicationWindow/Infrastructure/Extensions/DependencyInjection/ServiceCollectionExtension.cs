814bd3153aa3e4eac0e4999d6322c500ba5ac75b
Sullivan008 <kozakpeter0424@gmail.com>
2022-01-25 11:16:16 +0100

    * Implemented the CurrentApplicationWindowService to the Application.Client.Windows.ApplicationWindow project.

---
+++ Application.Client.Windows/Services/CurrentApplicationWindow/Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Application.Client.Windows.ApplicationWindow.Services.CurrentApplicationWindow.Interfaces;
+using Application.Client.Windows.ApplicationWindow.Window.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Client.Windows.ApplicationWindow.Services.CurrentApplicationWindow.Infrastructure.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddCurrentApplicationWindowService<TApplicationWindow>(this IServiceCollection @this) where TApplicationWindow : IApplicationWindow
+    {
+        @this.AddTransient<Func<TApplicationWindow, ICurrentApplicationWindowService>>(serviceProvider =>
+            applicationWindow => new CurrentApplicationWindowService(serviceProvider, applicationWindow));
+
+        return @this;
+    }
+}
---
+++ Application.Client.Windows/Services/CurrentApplicationWindow/Interfaces/ICurrentApplicationWindowService.cs
@@ -0,0 +1,11 @@
+﻿using Application.Client.Windows.ApplicationWindow.ViewModels.ApplicationWindowSettings.Initializers.Models.Interfaces;
+using Application.Client.Windows.ApplicationWindow.Window.Interfaces;
+
+namespace Application.Client.Windows.ApplicationWindow.Services.CurrentApplicationWindow.Interfaces;
+
+public interface ICurrentApplicationWindowService
+{
+    internal IApplicationWindow ApplicationWindow { get; }
+
+    public void ReInitializeWindowSettings(IApplicationWindowSettingsViewModelInitializerModel applicationWindowSettingsViewModelInitializerModel);
+}
