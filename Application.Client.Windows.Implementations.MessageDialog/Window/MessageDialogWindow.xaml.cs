3399abc8da0c92a507b20f159ff2653d70ec53f8
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-10 10:59:09 +0100

    * Modified the MessageDialog with the following logic:   * Binded the Title Property with the WindowSettings.Title.   * Set the NoResize mode.   * Hide the window icon.

--- Application.Client.Windows.Implementations.MessageDialog/Window/MessageDialogWindow.xaml.cs
+++ Application.Client.Windows.Implementations.MessageDialog/Window/MessageDialogWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace Application.Client.Windows.Implementations.MessageDialog.Window;
+﻿using Application.Client.Windows.Core.Window.Extensions;
+
+namespace Application.Client.Windows.Implementations.MessageDialog.Window;
 
 public partial class MessageDialogWindow : DialogWindow.Window.DialogWindow
 {
     public MessageDialogWindow()
     {
+        this.HideIcon();
+
         InitializeComponent();
     }
 }
