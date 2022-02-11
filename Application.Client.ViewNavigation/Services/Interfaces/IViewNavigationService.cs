d1604b5a46f9c3056c80fb02b355b4a23e9dd9c1
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-11 17:27:50 +0100

    * Upgraded the project references and renamed the project folders.

--- Application.Client.ViewNavigation/Services/Interfaces/IViewNavigationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.Client.Navigation.ViewNavigation.Abstractions.ViewModels;
-using Application.Client.Navigation.ViewNavigation.Abstractions.Windows;
-
-namespace Application.Client.Navigation.ViewNavigation.Services.Interfaces;
-
-public interface IViewNavigationService<TNavigationWindow> where TNavigationWindow : NavigationWindow
-{
-    public void Navigate<TPageViewModelType>() where TPageViewModelType : NavigationWindowPageViewModelBase<TNavigationWindow>;
-}
