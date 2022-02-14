b07e1ed55f39604dba04aa35046e50fb5dab3f18
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-14 17:59:35 +0100

    * Created the framework for the ExceptionDialogView.

---
+++ Application.Client.Windows.Implementations.ExceptionDialog/Window/Views/ExceptionDialog/ExceptionDialogView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Application.Client.Windows.Implementations.ExceptionDialog.Window.Views.ExceptionDialog;
+
+public partial class ExceptionDialogView : UserControl
+{
+    public ExceptionDialogView()
+    {
+        InitializeComponent();
+    }
+}
