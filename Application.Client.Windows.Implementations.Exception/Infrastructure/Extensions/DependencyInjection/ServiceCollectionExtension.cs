0a33cd69d8fe4a6e65bbf126a6cbacc33f32e95e
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-14 16:18:15 +0100

    * Created the framework of ExceptionWindow.

---
+++ Application.Client.Windows.Implementations.Exception/Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Client.Windows.Implementations.Exception.Infrastructure.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddExceptionWindow(this IServiceCollection @this)
+    {
+        return @this;
+    }
+}
---
+++ Application.Client.Windows.Implementations.Exception/Window/ExceptionWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Application.Client.Windows.Core.Window.Extensions;
+
+namespace Application.Client.Windows.Implementations.Exception.Window;
+
+public partial class ExceptionWindow : DialogWindow.Window.DialogWindow
+{
+    public ExceptionWindow()
+    {
+        this.HideIcon();
+
+        InitializeComponent();
+    }
+}
