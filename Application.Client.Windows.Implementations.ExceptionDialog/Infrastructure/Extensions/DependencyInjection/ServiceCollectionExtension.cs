65a3d709af2ed93534c040724329b444f7967ad1
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-14 16:27:47 +0100

    * Renamed the Application.Client.Windows.Implementations.Exception ot Application.Client.Windows.Implementations.ExceptionDialog.

---
+++ Application.Client.Windows.Implementations.ExceptionDialog/Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Client.Windows.Implementations.ExceptionDialog.Infrastructure.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddExceptionDialogWindow(this IServiceCollection @this)
+    {
+        return @this;
+    }
+}
---
+++ Application.Client.Windows.Implementations.ExceptionDialog/Window/ExceptionDialogWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Application.Client.Windows.Core.Window.Extensions;
+
+namespace Application.Client.Windows.Implementations.ExceptionDialog.Window;
+
+public partial class ExceptionDialogWindow : DialogWindow.Window.DialogWindow
+{
+    public ExceptionDialogWindow()
+    {
+        this.HideIcon();
+
+        InitializeComponent();
+    }
+}
