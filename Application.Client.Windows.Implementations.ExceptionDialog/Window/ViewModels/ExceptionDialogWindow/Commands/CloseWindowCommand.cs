8fa33df45f18e5371c2173b81e6ee9c3310e99ea
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-14 16:48:07 +0100

    * Created the CloseWindowCommand for ExceptionDialogWindow and implemented it where necessary

---
+++ Application.Client.Windows.Implementations.ExceptionDialog/Window/ViewModels/ExceptionDialogWindow/Commands/CloseWindowCommand.cs
@@ -0,0 +1,23 @@
+﻿using Application.Client.Common.Commands;
+using Application.Client.Windows.DialogWindow.Window.Interfaces;
+using Application.Client.Windows.Implementations.ExceptionDialog.Window.WindowResults.ExceptionDialog;
+using Application.Client.Windows.Implementations.ExceptionDialog.Window.WindowResults.ExceptionDialog.Enums;
+
+namespace Application.Client.Windows.Implementations.ExceptionDialog.Window.ViewModels.ExceptionDialogWindow.Commands;
+
+internal class CloseWindowCommand : AsyncCommandBase<ExceptionDialogWindowViewModel, IDialogWindow>
+{
+    public CloseWindowCommand(ExceptionDialogWindowViewModel callerViewModel) : base(callerViewModel)
+    { }
+
+    public override async Task ExecuteAsync(IDialogWindow window)
+    {
+        if (window.DialogResult.HasValue == false)
+        {
+            CallerViewModel.CustomDialogResult = new ExceptionDialogWindowResult { ExceptionDialogResult = ExceptionDialogResult.None };
+            window.DialogResult = false;
+        }
+
+        await Task.CompletedTask;
+    }
+}
--- Application.Client.Windows.Implementations.ExceptionDialog/Window/ViewModels/ExceptionDialogWindow/ExceptionDialogWindowViewModel.cs
+++ Application.Client.Windows.Implementations.ExceptionDialog/Window/ViewModels/ExceptionDialogWindow/ExceptionDialogWindowViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using Application.Client.Windows.DialogWindow.ViewModels.DialogWindow;
+using Application.Client.Windows.Implementations.ExceptionDialog.Window.ViewModels.ExceptionDialogWindow.Commands;
 using Application.Client.Windows.Implementations.ExceptionDialog.Window.ViewModels.ExceptionDialogWindowSettings;
 using Application.Client.Windows.Implementations.ExceptionDialog.Window.WindowResults.ExceptionDialog;
 
 namespace Application.Client.Windows.Implementations.ExceptionDialog.Window.ViewModels.ExceptionDialogWindow;
 
 public class ExceptionDialogWindowViewModel : DialogWindowViewModel<ExceptionDialogWindowSettingsViewModel, ExceptionDialogWindowResult>
-{ }
+{
+    public ExceptionDialogWindowViewModel()
+    {
+        CloseWindowCommand = new CloseWindowCommand(this);
+    }
+}
