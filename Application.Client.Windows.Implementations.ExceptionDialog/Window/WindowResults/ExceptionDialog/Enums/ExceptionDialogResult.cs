74154464b37da4382af13d94726258cd36ed78ef
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-14 16:30:50 +0100

    * Created the ExceptionDialogWindowResult with enum.

---
+++ Application.Client.Windows.Implementations.ExceptionDialog/Window/WindowResults/ExceptionDialog/Enums/ExceptionDialogResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Client.Windows.Implementations.ExceptionDialog.Window.WindowResults.ExceptionDialog.Enums;
+
+public enum ExceptionDialogResult
+{
+    None = 0,
+    Ok = 1
+}
---
+++ Application.Client.Windows.Implementations.ExceptionDialog/Window/WindowResults/ExceptionDialog/ExceptionDialogWindowResult.cs
@@ -0,0 +1,26 @@
+﻿using Application.Client.Windows.DialogWindow.Models.CustomDialogWindowResult;
+using Application.Client.Windows.Implementations.ExceptionDialog.Window.WindowResults.ExceptionDialog.Enums;
+using Application.Common.Utilities.Guard;
+
+namespace Application.Client.Windows.Implementations.ExceptionDialog.Window.WindowResults.ExceptionDialog;
+
+public class ExceptionDialogWindowResult : CustomDialogWindowResult
+{
+    public readonly ExceptionDialogResult? _exceptionDialogResult;
+    public ExceptionDialogResult ExceptionDialogResult
+    {
+        get
+        {
+            Guard.ThrowIfNotNull(_exceptionDialogResult, nameof(ExceptionDialogResult));
+
+            return _exceptionDialogResult!.Value;
+        }
+
+        init
+        {
+            Guard.ThrowIfNull(value, nameof(ExceptionDialogResult));
+
+            _exceptionDialogResult = value;
+        }
+    }
+}
