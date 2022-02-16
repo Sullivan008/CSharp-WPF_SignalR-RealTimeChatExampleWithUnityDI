1ed6c777d9918577d747efedbea82d8259c137e2
Sullivan008 <kozakpeter0424@gmail.com>
2022-02-16 11:40:05 +0100

    * Created the CloseWindowCommand for the MainWindow and binded it with CloseWindowEvent in MainWindow.xaml.

---
+++ Application.Client.Windows.Implementations.Main/Window/ViewModels/MainWindow/Commands/CloseWindowCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Client.Windows.Core.Commands.Abstractions;
+using Application.Client.Windows.NavigationWindow.Window.Interfaces;
+
+namespace Application.Client.Windows.Implementations.Main.Window.ViewModels.MainWindow.Commands;
+
+internal class CloseWindowCommand : AsyncWindowCommand<MainWindowViewModel, INavigationWindow>
+{
+    public CloseWindowCommand(MainWindowViewModel callerViewModel) : base(callerViewModel)
+    { }
+
+    public override async Task ExecuteAsync(INavigationWindow parameter)
+    {
+        await Task.CompletedTask;
+    }
+}
--- Application.Client.Windows.Implementations.Main/Window/ViewModels/MainWindow/MainWindowViewModel.cs
+++ Application.Client.Windows.Implementations.Main/Window/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -1,7 +1,13 @@
-﻿using Application.Client.Windows.Implementations.Main.Window.ViewModels.MainWindowSettings;
+﻿using Application.Client.Windows.Implementations.Main.Window.ViewModels.MainWindow.Commands;
+using Application.Client.Windows.Implementations.Main.Window.ViewModels.MainWindowSettings;
 using Application.Client.Windows.NavigationWindow.ViewModels.NavigationWindow;
 
 namespace Application.Client.Windows.Implementations.Main.Window.ViewModels.MainWindow;
 
 public class MainWindowViewModel : NavigationWindowViewModel<MainWindowSettingsViewModel>
-{ }
+{
+    public MainWindowViewModel()
+    {
+        CloseWindowCommand = new CloseWindowCommand(this);
+    }
+}
