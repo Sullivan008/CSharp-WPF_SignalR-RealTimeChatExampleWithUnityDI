5d68ad9fc25ce31efebfc05b1473b123ae695b68
Sullivan008 <kozakpeter0424@gmail.com>
2023-07-15 21:51:51 +0200

    * Remove AddMappersFromAssembly ServiceCollectionExtension method from Mapper.Core class library

--- SullyTech.Mapper.Core/Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ SullyTech.Mapper.Core/Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
-using SullyTech.Extensions.Enumerable;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SullyTech.Mapper.Core.Interfaces;
 
 namespace SullyTech.Mapper.Core.Infrastructure.Extensions.DependencyInjection;
@@ -12,27 +10,5 @@ public static class ServiceCollectionExtensions
         where TMapper : TIMapper
     {
         @this.AddTransient(typeof(TIMapper), typeof(TMapper));
-    }
-
-    public static IServiceCollection AddMappersFromAssembly(this IServiceCollection @this, Assembly assembly)
-    {
-        Type dataMapperType = typeof(IMapper<,>);
-
-        List<TypeInfo> implementationTypes =
-            assembly.DefinedTypes.Where(x => x.IsClass)
-                                 .Where(x => !x.IsAbstract)
-                                 .Where(x => x != dataMapperType)
-                                 .Where(x => x.GetInterfaces().Any(y => y.IsGenericType && y.GetGenericTypeDefinition() == dataMapperType))
-                                 .ToList();
-
-        implementationTypes.ForEach(implementationType =>
-        {
-            implementationType.ImplementedInterfaces.ForEach(implementedInterfaceType =>
-            {
-                @this.AddTransient(implementedInterfaceType, implementationType);
-            });
-        });
-
-        return @this;
     }
 }
